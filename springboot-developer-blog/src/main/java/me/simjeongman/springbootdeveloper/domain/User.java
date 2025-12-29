bcf0d70078ec06f92acdc0df0b3aa1b1103f3d96
Ssim02 <simbo@DESKTOP-2LQKBFH.localdomain>
2025-12-29 15:11:14 +0900

    save

--- springboot-developer-blog/src/main/java/me/simjeongman/springbootdeveloper/domain/User.java
+++ springboot-developer-blog/src/main/java/me/simjeongman/springbootdeveloper/domain/User.java
@@ -31,9 +31,10 @@ public class User implements UserDetails { // UserDeatils를 상속받아 인증
     private String password;
 
     @Builder
-    public User(String email, String password, String auth) {
+    public User(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
+        this.nickname = nickname;
     }
 
     @Override // 사용자가 가지고 있는 권한의 목록을 반환
@@ -79,6 +80,19 @@ public class User implements UserDetails { // UserDeatils를 상속받아 인증
     public boolean isEnabled() {
         // 계정이 사용 가능한지 확인하는 로직
         return true; // true -> 사용 가능
+    }
+
+    // 사용자 이름
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+
+
+
+    // 사용자 이름 변경
+    public User update(String nickname) {
+        this.nickname = nickname;
+
+        return this;
     }
 }
 
